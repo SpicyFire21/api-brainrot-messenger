553b9b34032330d7e89e9293de26919cb3a8926d
Adam KHNAGUI <khnagui.adam@gmail.com>
2025-11-23 14:08:06 +0100

    exposition de l'api a tt le monde

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ["http://localhost:5173", "capacitor://localhost"],
+    origin: ["*"],
     methods: ["GET", "POST"]
 }));
 
